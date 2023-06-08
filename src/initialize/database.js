8931dc638fb03c97bee37ab3be4071e3d647218c
Nightz <peerapat.ud@gmail.com>
2023-06-08 12:47:27 +0700

    make register, in processing make login , prepare mock data

--- src/initialize/database.js
+++ src/initialize/database.js
@@ -1,3 +1,37 @@
-const { sequelize } = require("../models");
+const { sequelize, User } = require("../models");
+const bcrypt = require("bcryptjs");
+const mockUsers = require("./mock_users.json");
 
-sequelize.sync({ force: true });
+const createMockData = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    // const password = bcrypt.hashSync("123456");
+    const users = mockUsers.map((user) => ({
+      ...user,
+      ...{ password: bcrypt.hashSync("123456") },
+    }));
+    console.log(users);
+    await User.bulkCreate(users);
+    process.exit(0);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+createMockData();
+
+// sequelize
+//   .sync({ force: true })
+//   .then(() => {
+//     return User.bulkCreate([
+//       { name: "Andy", password: hashed },
+//       { name: "Bobby", password: hashed },
+//       { name: "Candy", password: hashed },
+//       { name: "Danny", password: hashed },
+//       { name: "Eddy", password: hashed },
+//     ]);
+//   })
+//   .then(() => process.exit(0))
+//   .catch((err) => console.log(err.message));
+
+// sequelize.sync({ force: true });
