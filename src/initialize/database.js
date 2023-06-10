ec4de4258293261c56ade9c89401bffc2d4829a7
Nightz <peerapat.ud@gmail.com>
2023-06-10 21:53:23 +0700

    update mock data

--- src/initialize/database.js
+++ src/initialize/database.js
@@ -15,16 +15,29 @@ const createMockData = async () => {
 
     // console.log(users);
     await User.bulkCreate(users);
-    // await Book.bulkCreate(mockBooks);
     await Genre.bulkCreate(mockGenres);
+    await Book.bulkCreate(mockBooks);
+    // console.log(mockGenres);
     // await BookToGenre.bulkCreate(Book_to_Genre);
+
+    // for (const data of Book_to_Genre) {
+    //   // Find the book by title
+    //   const book = await Book.findOne({ where: { id: data.book_id } });
+
+    //   // Find the genre by name
+    //   const genre = await Genre.findOne({ where: { id: data.genre_id } });
+
+    //   // Create a record in the bookToGenre table
+    //   await BookToGenre.create({ bookId: book.id, genreId: genre.id });
+    // }
+
     process.exit(0);
   } catch (err) {
     console.log(err.message);
   }
 };
 
-// createMockData();
+createMockData();
 
 // console.log(mockBooks);
 // console.log(mockGenres);
