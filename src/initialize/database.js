64acfcc5b6f5aae8bc04b3f4434f9bdae0816a23
Nightz <peerapat.ud@gmail.com>
2023-06-10 18:30:52 +0700

    add book,genres,bookToGenre mockdata

--- src/initialize/database.js
+++ src/initialize/database.js
@@ -1,6 +1,9 @@
-const { sequelize, User } = require("../models");
+const { sequelize, User, Book, Genre, BookToGenre } = require("../models");
 const bcrypt = require("bcryptjs");
 const mockUsers = require("./mock_users.json");
+const mockBooks = require("./mock_books.json");
+const mockGenres = require("./genres.json");
+const Book_to_Genre = require("./BookToGenre.json");
 
 const createMockData = async () => {
   try {
@@ -9,12 +12,20 @@ const createMockData = async () => {
       ...user,
       ...{ password: bcrypt.hashSync("123456") },
     }));
+
     // console.log(users);
     await User.bulkCreate(users);
+    // await Book.bulkCreate(mockBooks);
+    await Genre.bulkCreate(mockGenres);
+    // await BookToGenre.bulkCreate(Book_to_Genre);
     process.exit(0);
   } catch (err) {
     console.log(err.message);
   }
 };
 
-createMockData();
+// createMockData();
+
+// console.log(mockBooks);
+// console.log(mockGenres);
+// console.log(Book_to_Genre);
